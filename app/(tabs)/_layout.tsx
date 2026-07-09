b93b82490f55dea6cec048fb570f5c9a7b3a4932
Mahdi Feyzi <mahdifeyzi84@gmail.com>
2026-07-09 18:50:30 +0330

    1405/04/18

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,16 +1,97 @@
-import { View, Text } from "react-native";
+import {View, Text, ImageBackground, Image} from "react-native";
 import React from "react";
-import { Tabs } from "expo-router";
+import {Tabs} from "expo-router";
+import {images} from "@/constants/images";
+import {icons} from "@/constants/icons";
+import "../global.css"
+
+
+const TabIcon = ({focused, icon, title}: any) => {
+    if (focused) {
+        return (
+            <ImageBackground source={images.highlight}
+                             className="flex flex-1 flex-row w-full gap-2 min-w-[112px] min-h-16 mt-[13px]  justify-center items-center rounded-full overflow-hidden">
+                <Image source={icon} tintColor="#151312" className="size-5"/>
+                <Text className="text-secondary text-base font-semibold">{title}</Text>
+            </ImageBackground>
+
+        )
+    }
+
+    return (
+        <View className="size-full justify-center items-center m-4 mt-2 rounded-full">
+            <Image source={icon} tintColor="#A8B5DB" className="size-5"/>
+
+        </View>
+    )
+}
 
 const _layout = () => {
-  return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
-        options={{ headerShown: false, title: "Home" }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs screenOptions={{
+            tabBarShowLabel: false,
+            tabBarItemStyle: {
+                width: "100%",
+                height: "100%",
+                justifyContent: 'center',
+                alignItems: "center",
+                display: "flex",
+            },
+            tabBarStyle: {
+                backgroundColor: "#0f0D23",
+                borderRadius: 50,
+                marginHorizontal: 20,
+                marginBottom: 36,
+                height: 52,
+                position: "absolute",
+                overflow: "hidden",
+                borderWidth: 1,
+                borderColor: "#0f0D23",
+            }
+        }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    headerShown: false, title: "Home",
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon focused={focused} icon={icons.home} title="Home"/>
+                    )
+                }}
+            />
+            <Tabs.Screen
+
+                name="search"
+                options={{
+                    title: "search",
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon focused={focused} icon={icons.search} title="Search"/>
+                    )
+
+                }}
+            />
+            <Tabs.Screen
+                name="saved"
+                options={{
+                    title: "saved",
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon focused={focused} icon={icons.save} title="Saved"/>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "profile",
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon focused={focused} icon={icons.person} title="Profile"/>
+                    )
+                }}
+            />
+        </Tabs>
+    );
 };
 
 export default _layout;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Stack } from "expo-router";
 import { StackScreen } from "react-native-screens";
+import "./global.css"
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+          name="(tabs)" options={{ headerShown: false }}
+      />
 
       <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
     </Stack>
